c96a179386707febb7b016aa0039797a8e604a98
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-09 13:51:05 +0200

    Added plugins and catppuccin.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,4 @@
-
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
