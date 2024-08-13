358b8e29c159d1dd8c65d41901a9ac0a2baf98cb
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-13 16:32:29 +0200

    added mason lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,17 @@
 return {
-    "williamboman/mason.nvim",
-    config = function ()
-        require('mason').setup()
-    end
+    {
+        "williamboman/mason.nvim",
+        config = function ()
+            require('mason').setup()
+        end
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        config = function ()
+            require('mason-lspconfig').setup({
+                ensure_installed = { 'lua_ls'}
+            })
+        end
+    }
 }
+
