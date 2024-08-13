00ba12a76dca40b895e3394c0e28af6614efdae0
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-13 17:49:36 +0200

    configured lsp for lua stable with telescope ui

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,16 @@ return {
                 ensure_installed = { 'lua_ls'}
             })
         end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        config = function ()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action,{})
+        end
     }
 }
 
