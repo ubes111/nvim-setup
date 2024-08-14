337c1e4295a6c5034dff432cb18999d58cae1338
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-14 02:02:02 +0200

    added python formaters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
