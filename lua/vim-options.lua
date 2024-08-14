b21def934e0e2c835b1a0b516e68ec3cd05b9d7d
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-15 00:00:04 +0200

    implemented gitsigns

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,15 +1,20 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Basic Neovim options
-vim.opt.number = true           -- Show line numbers
-vim.opt.relativenumber = true   -- Show relative line numbers
-vim.opt.hlsearch = true         -- Highlight search results
-vim.opt.incsearch = true        -- Show search results as you type
-vim.opt.expandtab = true        -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4          -- Number of spaces for each indentation
-vim.opt.tabstop = 4             -- Number of spaces per tab
-vim.opt.smartindent = true      -- Auto-indent new lines
-vim.opt.wrap = false            -- Disable line wrapping
-vim.opt.swapfile = false        -- Disable swapfile
-vim.opt.backup = false          -- Disable backup file
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.hlsearch = true -- Highlight search results
+vim.opt.incsearch = true -- Show search results as you type
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Number of spaces for each indentation
+vim.opt.tabstop = 4 -- Number of spaces per tab
+vim.opt.smartindent = true -- Auto-indent new lines
+vim.opt.wrap = false -- Disable line wrapping
+vim.opt.swapfile = false -- Disable swapfile
+vim.opt.backup = false -- Disable backup file
+
+
+-- Primes opts
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 
