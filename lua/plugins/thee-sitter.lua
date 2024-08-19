44381263a919e225826cb5a5f185caadc4df6bfb
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-19 02:35:14 +0200

    changed lualine to show buffers

--- lua/plugins/thee-sitter.lua
+++ lua/plugins/thee-sitter.lua
@@ -1,14 +1,13 @@
 return {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-          ensure_installed = { "python", "lua", "vim", "vimdoc", "query" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end
-    }
- 
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = { "python", "lua", "vim", "vimdoc", "query" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
