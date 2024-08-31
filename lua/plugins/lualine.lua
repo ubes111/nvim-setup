fd66a127d52caaa0564caadc79ee34ddfa87e562
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-08-31 03:44:13 +0200

    Canged lualine desighn to amazing

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,52 @@
---- TODO: add keymaps to manipulate buffers
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-            sections = { lualine_a = { 'buffers' } }
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = {'nvim-tree/nvim-web-devicons', 'folke/noice.nvim'}, -- List dependencies here
+    config = function()
+        require('lualine').setup {
+            options = {
+                icons_enabled = true,
+                component_separators = '|',
+                section_separators = '',
+            },
+            sections = {
+                lualine_x = {
+                    {
+                        function()
+                            return require("noice").api.statusline.mode.get()
+                        end,
+                        cond = function()
+                            return require("noice").api.statusline.mode.has()
+                        end,
+                        color = { fg = "#ff9e64" },
+                    },
+                    {
+                        function()
+                            return require("noice").api.status.command.get()
+                        end,
+                        cond = function()
+                            return require("noice").api.status.command.has()
+                        end,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+                lualine_a = {
+                    'buffers',
+                },
+                lualine_b = {
+                    'branch',
+                    'diff',
+                    'diagnostics',
+                },
+                lualine_c = {
+                    'filename',
+                },
+                lualine_y = {
+                    'progress',
+                },
+                lualine_z = {
+                    'location',
+                },
+            }
+        }
+    end,
 }
