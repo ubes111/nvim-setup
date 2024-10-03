36e3823483676daaedcb8946a76ded2537b85af8
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-10-04 01:23:23 +0200

    Added dotfiles to telescope ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,13 @@ return {
         tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
+            require("telescope").setup({
+                pickers = {
+                    find_files = {
+                        find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+                    },
+                },
+            })
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-p>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
