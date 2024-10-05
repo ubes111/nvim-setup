0751f761f9ded249367ad79c8ad5b4053520e9af
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-10-05 02:04:11 +0200

    Fixed rust linter and added json

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         'williamboman/mason-lspconfig.nvim',
         config = function ()
             require('mason-lspconfig').setup({
-                ensure_installed = { 'lua_ls', 'pyright', 'rust_analyzer'}
+                ensure_installed = { 'lua_ls', 'pyright', 'rust_analyzer', 'jsonls'}
             })
         end
     },
@@ -19,6 +19,8 @@ return {
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup({})
             lspconfig.pyright.setup({})
+            lspconfig.rust_analyzer.setup({})
+            lspconfig.jsonls.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action,{})
