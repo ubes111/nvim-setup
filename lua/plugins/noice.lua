c8b8b3b59c159c5286407b09b4b0268a8d5f2746
Kyryl Kurkin <ryl.kurkin@gmail.com>
2024-10-10 01:59:09 +0200

    Fixed bug with: transparent bg in formatter progress bar

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,23 +1,30 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any Noice options here if needed
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
-  config = function(_, opts)
-    -- Setup for Noice
-    require("noice").setup(opts)
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        views = {
+            mini = {
+                win_options = {
+                    winblend = 0, -- Set winblend to 0 for the mini view
+                },
+            },
+        },
+        -- Add any additional Noice options here if needed
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+    },
+    config = function(_, opts)
+        -- Setup for Noice
+        require("noice").setup(opts)
 
-    -- Setup for nvim-notify, only setting background_colour to avoid transparency issue
-    require('notify').setup({
-      background_colour = "#000000"  -- Set background color to fix transparency issue
-    })
+        -- Setup for nvim-notify, only setting background_colour to avoid transparency issue
+        require('notify').setup({
+            background_colour = "#000000" -- Set background color to fix transparency issue
+        })
 
-    -- Route Noice notifications to nvim-notify
-    vim.notify = require("notify")
-  end,
+        -- Route Noice notifications to nvim-notify
+        vim.notify = require("notify")
+    end,
 }
